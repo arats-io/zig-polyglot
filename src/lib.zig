e0168fe2304a901fe662872762cf1b9047a49662
Nicolae Nicora <nicolae.nicora@yahoo.com>
2023-10-14 21:21:57 +0200

    first version of encoder and decoder for frpc
    Signed-off-by: Nicolae Nicora <nicolae.nicora@yahoo.com>

---
+++ src/lib.zig
@@ -0,0 +1,6 @@
+pub const Decode = @import("decode.zig");
+pub const Decoder = @import("decoder.zig");
+pub const Encode = @import("encode.zig");
+pub const Encoder = @import("encoder.zig");
+
+pub const Kind = @import("types.zig").Kind;
